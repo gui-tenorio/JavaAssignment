ec01e4d6d802d033f06647f63b16e7efd5beb5a2
Yow <Yow@192.168.0.165>
2020-03-22 15:12:07 +0000

    create country class and continent enum

---
+++ CAoodp/src/CAoodp/Continent.java
@@ -0,0 +1,22 @@
+package CAoodp;
+
+public enum Continent {
+	ASIA("Asia"),
+	EUROPE("Europe"),
+	NORTH_AMERICA("North America"),
+	AFRICA("Africa"),
+	OCEANIA("Oceania"),
+	ANTARTICA("Antartica"),
+	SOUTH_AMERICA("South America");
+	
+	private final String name;
+	
+	Continent(String name){
+		this.name = name;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+
+}
